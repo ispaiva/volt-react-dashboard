8285e4399ebf03b0b9be91ccdf51871cffc6b3fa
Igor Paiva <igor.s.paiva@gmail.com>
2023-08-16 12:56:34 -0300

    primeiro commit

--- src/data/transactions.js
+++ src/data/transactions.js
@@ -6,7 +6,7 @@ export default [
         "invoiceNumber": 300500,
         "status": "Paid",
         "subscription": "Platinum Subscription Plan",
-        "price": "799,00",
+        "price": "1799,00",
         "issueDate": moment().subtract(1, "days").format("DD MMM YYYY"),
         "dueDate": moment().subtract(1, "days").add(1, "month").format("DD MMM YYYY")
     },
--- src/pages/dashboard/DashboardOverview.js
+++ src/pages/dashboard/DashboardOverview.js
@@ -45,14 +45,14 @@ export default () => {
         <Col xs={12} className="mb-4 d-none d-sm-block">
           <SalesValueWidget
             title="Sales Value"
-            value="10,567"
+            value="10,557"
             percentage={10.57}
           />
         </Col>
         <Col xs={12} className="mb-4 d-sm-none">
           <SalesValueWidgetPhone
             title="Sales Value"
-            value="10,567"
+            value="10,557"
             percentage={10.57}
           />
         </Col>
@@ -65,12 +65,13 @@ export default () => {
             icon={faChartLine}
             iconColor="shape-secondary"
           />
+
         </Col>
 
         <Col xs={12} sm={6} xl={4} className="mb-4">
           <CounterWidget
             category="Revenue"
-            title="$43,594"
+            title="$43,555"
             period="Feb 1 - Apr 1"
             percentage={28.4}
             icon={faCashRegister}
